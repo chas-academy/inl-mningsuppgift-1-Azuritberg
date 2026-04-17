a58f3c4c9c5bd8f4673d9faea766f0e021ae70e5
bojanalukacgmailcom <bojana.lukac@gmail.com>
2026-04-17 17:15:51 +0200

    nu så..så många fel det blir när man läster snabbt och felstavar påvägen till rätt kod :))

--- src/script.js
+++ src/script.js
@@ -39,7 +39,7 @@ function showContacts(contactBook, groupToShow) { // Rör ej denna kod
 
     for (const contact of contactBook.contacts) {
         if (contact.group === groupToShow) {
-            return `Name: ${contact.name}, Telefon: ${contact.phone}`; // här ska sträng med namn och tekefonnummer visas i form av string interpolation  
+            return `Namn: ${contact.name}, Telefon: ${contact.phone}`; // här ska sträng med namn och tekefonnummer visas i form av string interpolation  
         }    
     }
   
